a732f976ea803bddadce3610fd9ffbf777e64233
TaikiFnit <taikifnitspg@gmail.com>
2018-01-30 23:21:35 +0900

    complete fetch registers from api

--- espanol-mecanografia/src/App.js
+++ espanol-mecanografia/src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import request from 'superagent';
+import { link } from 'fs';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {"registers": {}}
+  }
+
+  componentDidMount() {
+    const url = 'http://localhost:8080/registers'
+    request
+      .get(url)
+      .end((err, res) => {
+        console.log(err);
+        console.log(res.body);
+        if(res.body.results) {
+          this.setState({"registers": res.body.results})
+        }
+      })
+  }
+
   render() {
+
+    let register_list = []
+
+    Object.keys(this.state.registers).forEach((key)=> {
+      register_list.push(<li>{key}</li>)
+    })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ul>
+        {register_list}
+      </ul>
     );
   }
 }
