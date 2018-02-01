dfc873502aadd126e1a121f67e8e587de23b8786
TaikiFnit <taikifnitspg@gmail.com>
2018-02-01 11:57:37 +0900

    apply material ui v0.x

--- espanol-mecanografia/src/App.js
+++ espanol-mecanografia/src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 import request from 'superagent';
 import { link, lstat } from 'fs';
 
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import {pinkA200, white, darkBlack, cyan500,  fullBlack, fullWhite, grey100, grey500, grey300, grey900, grey800, grey700} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import AppBar from 'material-ui/AppBar';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import {AppBar, IconButton, IconMenu, MenuItem} from 'material-ui';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
+import merge from 'lodash.merge';
+
+const muiTheme = {palette: {primary1Color: grey900, primary2Color: grey900, primary3Color: 800}}
 
 class App extends Component {
 
@@ -42,8 +51,29 @@ class App extends Component {
     })
 
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <AppBar title="Español Mecanografía" />
+      <MuiThemeProvider muiTheme={getMuiTheme(merge(darkBaseTheme, muiTheme))}>
+        <AppBar title="Español Mecanografía" style={{marginBottom: 60, color: fullWhite}} titleStyle={{color: fullWhite}}/>
+
+        <Card style={{background: grey800}} >
+          <CardHeader
+            title="URL Avatar"
+            subtitle="Subtitle"
+            avatar="images/jsa-128.jpg"
+          />
+
+          <CardTitle title="Card title" subtitle="Card subtitle" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+            <FlatButton label="Action1" />
+            <FlatButton label="Action2" />
+          </CardActions>
+        </Card>
+
       </MuiThemeProvider>
     );
   }
